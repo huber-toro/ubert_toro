51f86be4892dbff09c45e7aad3d747d4b20723a4
Ubert <alonso.toro@est.iudigital.edu.co>
2026-01-27 15:54:27 +0000

    correccion ruta linea 5 ejecucion

---
+++ .github/workflows/src/pad_2026/actividad_1.py
@@ -0,0 +1,33 @@
+# Evidencia de aprendizaje Clases, Objetos y archivos de datos en Python
+# Ubert Toro - IUDIGITAL
+
+import requests
+import json
+import os
+
+class Ingestiones():
+    def __init__(self):
+        self.ruta_static = "src/pad_2026/static/"
+        
+    def leer_api(self, url="http://api.citybik.es/v2/networks"):
+        response = requests.get(url)
+        return response.json()
+
+    def escribir_json(self, nombre, datos):
+        ruta_json = "{}/Json/{}".format(self.ruta_static, nombre)
+        os.makedirs(os.path.dirname(ruta_json), exist_ok=True)
+        with open(ruta_json, "w", encoding="utf-8") as f:
+            json.dump(datos, f, indent=4, ensure_ascii=False)
+
+# Uso
+inges = Ingestiones()
+datos_api = inges.leer_api()
+print(datos_api["networks"][:5])  # muestra solo los primeros 5 sistemas
+
+inges.escribir_json("Actividad_1.json", datos_api)
+
+
+
+
+
+
--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pad",
+    name="pad_2026",
     version="0.0.1",
     author="ubert toro",
     author_email="alonso.toro@est.iudigital.edu.co",
-    description="Evidencia de aprendizaje",    
-    py_modules=["actividad_1"],
+    description="Evidencia de aprendizaje",
+    py_modules=["Entregable actividad 1"],
     install_requires=[
         "pandas",
         "requests",
+        "openpyxl",
         "matplotlib"
     ]
     
